baa2136e8a57a9ba6af7a30933daf25dff630301
T.Ádám <78810601+Mikihiko01@users.noreply.github.com>
2022-03-09 11:54:03 +0100

    3.név dekrelálása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -4,8 +4,10 @@ package konfliktuskezeles;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
+        int i= 7;
       String nev1;
       String nev2="Ádám";
+        int nev3;
     }
     
 }
